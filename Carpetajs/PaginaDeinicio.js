c7492fd928b5573117658b21e093333205567b58
Ivarchow <89149103+Ivarchow@users.noreply.github.com>
2021-10-06 12:20:03 -0500

    cambioivar

--- Carpetajs/PaginaDeinicio.js
+++ Carpetajs/PaginaDeinicio.js
@@ -1,12 +1,31 @@
 let animado = document.querySelectorAll(".animado");
+
+let btnanimado = document.querySelectorAll("btnanimado")
+
+window.addEventListener('scroll', mostrarScroll);
+
+window.addEventListener('scroll', mostrarbtn);
+
 function mostrarScroll(){
     let scrollTop = document.documentElement.scrollTop;
+
     for(var i=0; i<animado.length; i++){
         let alturaAnimado = animado[i].offsetTop;
-        if(alturaAnimado+300<scrollTop){
+        if(alturaAnimado+400<scrollTop){
             animado[i].style.opacity = 1;
             animado[i].classList.add("mostrarArriba");
         }
     }
 }
-window.addEventListener('scroll', mostrarScroll);
+
+function mostrarbtn(){
+    let scrollTop = document.documentElement.scrollTop;
+
+    for(var i=0; i<animado.length; i++){
+        let alturaAnimado = animado[i].offsetTop;
+        if(alturaAnimado+400<scrollTop){
+            animado[i].style.opacity = 1;
+            animado[i].classList.add("mostrarArriba");
+        }
+    }
+}
