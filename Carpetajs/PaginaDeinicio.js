5a23ea57f2202cf35b6d6f58574189c0d09b9194
Ivarchow <89149103+Ivarchow@users.noreply.github.com>
2021-10-05 22:40:53 -0500

    cambioivar

--- Carpetajs/PaginaDeinicio.js
+++ Carpetajs/PaginaDeinicio.js
@@ -3,7 +3,7 @@ function mostrarScroll(){
     let scrollTop = document.documentElement.scrollTop;
     for(var i=0; i<animado.length; i++){
         let alturaAnimado = animado[i].offsetTop;
-        if(alturaAnimado+600<scrollTop){
+        if(alturaAnimado+300<scrollTop){
             animado[i].style.opacity = 1;
             animado[i].classList.add("mostrarArriba");
         }
