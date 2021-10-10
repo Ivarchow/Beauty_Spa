f9721c8dbeee871a94a043b24309584d4ab83a20
Ivarchow <89149103+Ivarchow@users.noreply.github.com>
2021-10-10 18:36:01 -0500

    cambiosivar

--- CarpetaJs/FetchCartas.js
+++ CarpetaJs/FetchCartas.js
@@ -5,7 +5,8 @@ const URLS= ['https://jsonplaceholder.typicode.com/photos/1',
 'https://jsonplaceholder.typicode.com/photos/4',
 'https://jsonplaceholder.typicode.com/photos/5'];
 
-const arrIds=["nails","skin","pest","dato-user4","dato-user5"];
+const arrIds=["carta1","carta2","carta3"];
+const arrSErvicios=["nails","skin","pest"];
 
 var varCtrlUrlsCol = 0, varCtlrUrlsRow=0;
 for(varCtrlUrlsCol; varCtrlUrlsCol < URLS.length; varCtrlUrlsCol++){
@@ -14,11 +15,33 @@ fetch(URLS[varCtrlUrlsCol])
     .then(carta => carta.json())
     .then(carta =>{
 
-            let elemento = document.getElementById(arrIds[varCtlrUrlsRow]);
+/*            let varCtrlCard=0;
+            for(varCtrlCard;varCtrlCard<arrIds.length;varCtrlCard++){
+                let col,CardsNum, elemento;
+                col = document.createElement('div');
+                col.class = "col";
+                col.style.paddingBottom = "2%";
+                col.id = arrIds[varCtlrUrlsRow];
+
+                
+                document.getElementsById("nails").appendChild(col);
+
+            }*/
+
+            let col = document.createElement('div');
+             col.class = "col";
+             col.style.paddingBottom = "2%";
+             col.id = "carta1";
+             document.getElementById("nails").appendChild(col);
+
+             let col1 = document.createElement('div');
+             col1.class = "col";
+             col1.style.paddingBottom = "2%";
+             col1.id = "carta2";
+             document.getElementById("nails").appendChild(col1);
+
+            let elemento = document.getElementById(col.id);
             elemento.innerHTML = 
-
-
-            '<div class="col" style="padding-bottom: 2%" id="dato-user1">\n' +
             '<figure class="image-block" style="margin: auto;">\n' +
                 '<h1>'+ carta.id+'</h1>\n' +
                 '<img src="'+carta.url +'"/>\n' +
@@ -34,20 +57,29 @@ fetch(URLS[varCtrlUrlsCol])
                         'Reservar\n' +
                     '</button>\n' +
                 '</figcaption>\n' +
-            '</figure>'
-            '</div>';
-            
-            /*'<div class="card" style="width: 18rem;">\n' +
-            '    <img src="'+user.url+'" class="card-img-top" alt="image">\n' +
-            '    <div class="card-body">\n' +
-            '        <h5 class="card-title">'+user.id+'</h5>\n' +
-            '        <h5 class="card-title">PRECIO: '+user.albumId+'</h5>\n' +
-            '        <p class="card-text">'+user.title+'</p>\n' +
-            '        <a href="#" class="btn btn-primary">Add</a>\n' +
-            '    </div>\n' +
-            '</div>\n' +
-            '<br/>';*/
+            '</figure>';
+
+            let elemento1 = document.getElementById(col1.id);
+            elemento1.innerHTML = 
+            '<figure class="image-block" style="margin: auto;">\n' +
+                '<h1>'+ carta.id+'</h1>\n' +
+                '<img src="'+carta.url +'"/>\n' +
+                '<figcaption>\n' +
+                    '<h3>\n' +
+                        'Ver Más\n' +
+                    '</h3>\n' +
+                    '<div class="overflow-auto example" style="height: 250px; ">\n' +
+                        '<p>Precio: '+carta.albumId+'$</p>\n' +
+                        '<p>'+carta.title+'</p>\n' +
+                    '</div>\n' +
+                    '<button>\n' +
+                        'Reservar\n' +
+                    '</button>\n' +
+                '</figcaption>\n' +
+            '</figure>';
+        
             varCtlrUrlsRow++;
+            
     })
     .catch(err => console.log(err))
 }
