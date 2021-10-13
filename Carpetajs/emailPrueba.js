3a537362cfcaaa2e7e0591f131c773bf349d480f
R4f4S4lc3d0 <rafa22524@gmail.com>
2021-10-12 21:35:52 -0500

    Revert "cambios"
    This reverts commit c858c174b0ed04a116a86b2d783e8e194869b74f.

--- Carpetajs/emailPrueba.js
+++ Carpetajs/emailPrueba.js
@@ -1,13 +1,78 @@
-function sendMail() {
+const inputs = document.querySelector('form')
+function sendEmail() {
   Email.send({
       Host : "smtp.mailtrap.io",
-      Username : "818ba3fd56eb89",
-      Password : "b674ef50bc59ff",
-      To : 'rafael.salcedosandoval11@gmail.com',
-      From : "rafael.salcedosandoval11@gmail.com",
+      Username : "71e4b9403ab5c0",
+      Password : "f932ba1e05391f",
+      To : "divinitebeautyspa@gmail.com",
+      From : inputs.elements["email"].value,
       Subject : "Test email",
-      Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
-  }).then(
-    message => alert(message)
-  );
+      
+      Body:  inputs.elements["name"].value + " con el correo: "+ inputs.elements["email"].value+ " te envió el siguiente mensaje: "+"<br>" +inputs.elements["message"].value 
+    }).then(message =>{
+      let p;
+      if(message!="OK"){
+        p = document.getElementById('alert');
+        p.removeAttribute("hidden")
+        
+        setTimeout(() => {
+          window.location.reload();
+         }, 4000);
+      }else{
+        console.log(message)
+        p = document.getElementById('goodAlert')
+        p.removeAttribute("hidden")
+        
+       setTimeout(() => {
+        window.location.reload();
+       }, 3000);
+        
+      }
+    })
   }
+
+/*const btn = document.querySelector('button')
+const inputs = document.querySelector('form')
+btn.addEventListener('click', () => {
+  Email.send({
+    Host: "smtp.mailtrap.io",
+    Username: "71e4b9403ab5c0",
+    Password: "f932ba1e05391f",
+    To: "divinitebeautyspa@gmail.com",
+    From: inputs.elements["email"].value,
+    Subject: "Contact Us Beauty Spa", 
+    Body: inputs.elements["message"].value + "<br>" + inputs.elements["name"]
+  }).then(msg => alert("The email was successfully sent"))
+})*/
+
+
+
+//Código para enviar correos
+
+/*(function () {
+  emailjs.init("user_nsfB75HsHb3gAeJ6RzBYc");
+})();
+
+function sendMail() {
+  let fullName = document.getElementById("name").value;
+  let userEmail = document.getElementById("email").value;
+  let userMessage = document.getElementById("message").value;
+
+  var contactParams = {
+    from_name: fullName,
+    from_email: userEmail,
+    message: userMessage,
+  };
+
+  emailjs
+    .send("service_l8t7c6q", "template_74px0yb", contactParams)
+    .then(function (res) {});
+
+  return true;
+}
+*/
+ function envi() {
+  
+  window.location.reload();
+}
+
