3a8cec4558b279e7d4f04c3e1b9203830a976a9c
Ivarchow <89149103+Ivarchow@users.noreply.github.com>
2021-10-13 14:27:12 -0500

    cambioivar

--- CarpetaJs/FetchCartas.js
+++ CarpetaJs/FetchCartas.js
@@ -29,7 +29,7 @@ function generatorCols(){
     
         let varCtrlCols=1, varCtlrContinue=5;
         
-    for(let varCtlrService=0; varCtlrService <3; varCtlrService++ ){
+    for(let varCtlrService=0; varCtlrService < arrServicios.length; varCtlrService++ ){
 
         console.log(arrServicios[varCtlrService]);
 
@@ -80,7 +80,7 @@ function getCartas(){
                     '</figcaption>\n' +
                 '</figure>';
                 varCtlrCards++;
-        }
+            }
         if(Servicios.Categoria=="Cuidado de la piel"){
             let elemento1 = document.getElementById(arrIds[varCtlrCards]);
             elemento1.innerHTML = 
@@ -105,6 +105,111 @@ function getCartas(){
         if(Servicios.Categoria=="Faciales"){
             let elemento2 = document.getElementById(arrIds[varCtlrCards]);
             elemento2.innerHTML = 
+            '<figure class="image-block" style="margin: auto;">\n' +
+                '<h1>'+ Servicios.titulo+'</h1>\n' +
+                '<img src="'+Servicios.img+'"/>\n' +
+                '<figcaption>\n' +
+                    '<h3>\n' +
+                        'Ver Más\n' +
+                    '</h3>\n' +
+                    '<div class="overflow-auto example" style="height: 250px; ">\n' +
+                        '<p>Precio: '+Servicios.precio+'$</p>\n' +
+                        '<p>'+Servicios.Descripcion+'</p>\n' +
+                    '</div>\n' +
+                    '<button>\n' +
+                        'Reservar\n' +
+                    '</button>\n' +
+                '</figcaption>\n' +
+            '</figure>';
+            varCtlrCards++;
+        }
+        if(Servicios.Categoria=="Pedicure y manicure"){
+            let elemento3 = document.getElementById(arrIds[varCtlrCards]);
+            elemento3.innerHTML = 
+            '<figure class="image-block" style="margin: auto;">\n' +
+                '<h1>'+ Servicios.titulo+'</h1>\n' +
+                '<img src="'+Servicios.img+'"/>\n' +
+                '<figcaption>\n' +
+                    '<h3>\n' +
+                        'Ver Más\n' +
+                    '</h3>\n' +
+                    '<div class="overflow-auto example" style="height: 250px; ">\n' +
+                        '<p>Precio: '+Servicios.precio+'$</p>\n' +
+                        '<p>'+Servicios.Descripcion+'</p>\n' +
+                    '</div>\n' +
+                    '<button>\n' +
+                        'Reservar\n' +
+                    '</button>\n' +
+                '</figcaption>\n' +
+            '</figure>';
+            varCtlrCards++;
+        }
+        if(Servicios.Categoria=="Maquillaje"){
+            let elemento4 = document.getElementById(arrIds[varCtlrCards]);
+            elemento4.innerHTML = 
+            '<figure class="image-block" style="margin: auto;">\n' +
+                '<h1>'+ Servicios.titulo+'</h1>\n' +
+                '<img src="'+Servicios.img+'"/>\n' +
+                '<figcaption>\n' +
+                    '<h3>\n' +
+                        'Ver Más\n' +
+                    '</h3>\n' +
+                    '<div class="overflow-auto example" style="height: 250px; ">\n' +
+                        '<p>Precio: '+Servicios.precio+'$</p>\n' +
+                        '<p>'+Servicios.Descripcion+'</p>\n' +
+                    '</div>\n' +
+                    '<button>\n' +
+                        'Reservar\n' +
+                    '</button>\n' +
+                '</figcaption>\n' +
+            '</figure>';
+            varCtlrCards++;
+        }
+        if(Servicios.Categoria=="Estetica"){
+            let elemento5 = document.getElementById(arrIds[varCtlrCards]);
+            elemento5.innerHTML = 
+            '<figure class="image-block" style="margin: auto;">\n' +
+                '<h1>'+ Servicios.titulo+'</h1>\n' +
+                '<img src="'+Servicios.img+'"/>\n' +
+                '<figcaption>\n' +
+                    '<h3>\n' +
+                        'Ver Más\n' +
+                    '</h3>\n' +
+                    '<div class="overflow-auto example" style="height: 250px; ">\n' +
+                        '<p>Precio: '+Servicios.precio+'$</p>\n' +
+                        '<p>'+Servicios.Descripcion+'</p>\n' +
+                    '</div>\n' +
+                    '<button>\n' +
+                        'Reservar\n' +
+                    '</button>\n' +
+                '</figcaption>\n' +
+            '</figure>';
+            varCtlrCards++;
+        }
+        if(Servicios.Categoria=="Masajes"){
+            let elemento6 = document.getElementById(arrIds[varCtlrCards]);
+            elemento6.innerHTML = 
+            '<figure class="image-block" style="margin: auto;">\n' +
+                '<h1>'+ Servicios.titulo+'</h1>\n' +
+                '<img src="'+Servicios.img+'"/>\n' +
+                '<figcaption>\n' +
+                    '<h3>\n' +
+                        'Ver Más\n' +
+                    '</h3>\n' +
+                    '<div class="overflow-auto example" style="height: 250px; ">\n' +
+                        '<p>Precio: '+Servicios.precio+'$</p>\n' +
+                        '<p>'+Servicios.Descripcion+'</p>\n' +
+                    '</div>\n' +
+                    '<button>\n' +
+                        'Reservar\n' +
+                    '</button>\n' +
+                '</figcaption>\n' +
+            '</figure>';
+            varCtlrCards++;
+        }
+        if(Servicios.Categoria=="Pestañas y cejas"){
+            let elemento7 = document.getElementById(arrIds[varCtlrCards]);
+            elemento7.innerHTML = 
             '<figure class="image-block" style="margin: auto;">\n' +
                 '<h1>'+ Servicios.titulo+'</h1>\n' +
                 '<img src="'+Servicios.img+'"/>\n' +
