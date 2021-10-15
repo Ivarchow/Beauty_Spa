966a1a79b6204f0b998afd629ec87aa23472cf17
Ivarchow <89149103+Ivarchow@users.noreply.github.com>
2021-10-14 22:18:55 -0500

    cambio ivar

--- Carpetajs/FetchCarta.js
+++ Carpetajs/FetchCarta.js
@@ -1,29 +1,94 @@
-window.onload=() =>{
 
     const URLS= [];
     const arrIds=[];
     const arrServicios=["nails","skin","pest","fac","pedi","makeup","estetica","massage"];
-    
+
+    window.onload=() =>{   
     main();
-    
+    };
+
     function main(){
-    
-        generatorArrys();
-        generatorCols();
-        getCartas();
+        let banderArrays=0;
+        countServicesLenght();
+        //generatorArrys(35);
+        if(banderArrays==1){
+            countServices();
+        }
+        
+        //generatorCols();
+        //getCartas();
     }
     
-    function generatorArrys(){
-        for(let varCtlrGenerator = 1;varCtlrGenerator<=35;varCtlrGenerator++){
+    function generatorArrys(tamañoJson){
+        for(let varCtlrGenerator = 1; varCtlrGenerator<=tamañoJson; varCtlrGenerator++){
             let url = "http://localhost:3000/Servicios/" + varCtlrGenerator;
+            let ids = "carta" + varCtlrGenerator;
             URLS.push(url);
-        }
-        for(let varCtlrIdsCards=0; varCtlrIdsCards<=35;varCtlrIdsCards++){
-            let ids = "carta" + varCtlrIdsCards;
             arrIds.push(ids);
         }
         console.log(arrIds);
+        console.log(URLS);
+        banderArrays=1;
     }
+
+function countServicesLenght(){
+        let Jsonlength=0;
+        fetch("http://localhost:3000/Servicios")
+        .then(response1 => response1.json())
+        .then(TodosLosServicios =>{
+
+                Jsonlength = Object.keys(TodosLosServicios).length;
+                console.log(Jsonlength);
+                generatorArrys(Jsonlength);
+        })
+        .catch(err1 => console.log(err1));
+}
+
+function countServices(){
+        let Servicio1=0,Servicio2=0,Servicio3=0,Servicio4=0,Servicio5=0,Servicio6=0,Servicio7=0,Servicio8=0;
+    
+        for(let varCtrlFetch=0;varCtrlFetch<URLS.length;varCtrlFetch++ ){
+    
+        fetch(URLS[varCtrlFetch])
+        .then(response => response.json())
+        .then(Servicios =>{
+
+            if(Servicios.Categoria == "Unas"){
+                Servicio1++;
+                console.log(Servicio1);
+            }
+            if(Servicios.Categoria == "Cuidado de la piel"){
+                Servicio2++;
+                console.log(Servicio2);
+            }
+            if(Servicios.Categoria == "Pestañas y cejas"){
+                Servicio3++;
+                console.log(Servicio3);
+            }
+            if(Servicios.Categoria == "Faciales"){
+                Servicio4++;
+                console.log(Servicio4);
+            }
+            if(Servicios.Categoria == "Pedicure y manicure"){
+                Servicio5++;
+                console.log(Servicio5);
+            }
+            if(Servicios.Categoria == "Maquillaje"){
+                Servicio6++;
+                console.log(Servicio6);
+            }
+            if(Servicios.Categoria == "Estetica"){
+                Servicio7++;
+                console.log(Servicio7);
+            }
+            if(Servicios.Categoria == "Masajes"){
+                Servicio8++;
+                console.log(Servicio8);
+            }
+        })
+        .catch(err => console.log(err));
+    } 
+}
     
     function generatorCols(){
         
@@ -50,8 +115,10 @@ window.onload=() =>{
             }
         }
     }
+
+
     
-    function getCartas(){
+/*    function getCartas(){
         let varCtlrCards=1;
     
         for(let varCtrlFetch=0;varCtrlFetch<URLS.length;varCtrlFetch++ ){
@@ -231,37 +298,4 @@ window.onload=() =>{
         })
         .catch(err => console.log(err));
     }
-    }
-    };
-    /*window.onload=() =>{
-    
-                    let col = document.createElement('div');
-                    col.className = "col";
-                    col.style.paddingBottom = "2%";
-                    col.id = arrIds[varCtlrCards];
-                    console.log(col);
-                    document.getElementById(arrSErvicios[varCtlrService]).appendChild(col);  
-    
-    
-                    let elemento = document.getElementById(col.id);
-                elemento.innerHTML = 
-                '<figure class="image-block" style="margin: auto;">\n' +
-                    '<h1>'+ Servicios.titulo+'</h1>\n' +
-                    '<img src="'+Servicios.img+'"/>\n' +
-                    '<figcaption>\n' +
-                        '<h3>\n' +
-                            'Ver Más\n' +
-                        '</h3>\n' +
-                        '<div class="overflow-auto example" style="height: 140px; ">\n' +
-                            '<p>Precio: '+Servicios.precio+'$</p>\n' +
-                            '<p>'+Servicios.Descripcion+'</p>\n' +
-                        '</div>\n' +
-                        '<button>\n' +
-                            'Reservar\n' +
-                        '</button>\n' +
-                    '</figcaption>\n' +
-                '</figure>';
-    
-    
-    
-            }*/
+}*/
