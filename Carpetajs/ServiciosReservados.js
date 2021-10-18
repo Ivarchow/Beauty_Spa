4c8fc37f5f89b88776949cdd2f266ad3b68e72fb
Ivarchow <89149103+Ivarchow@users.noreply.github.com>
2021-10-18 15:14:25 -0500

    cambio ivar filtrado de resvervaciones

--- Carpetajs/ServiciosReservados.js
+++ Carpetajs/ServiciosReservados.js
@@ -1,6 +1,7 @@
 window.onload = ()=>{
+
 let cuerpo = document.querySelector('body');
-let btnReservar = document.querySelector("#btnReservar");
+let btnReservar = document.querySelector("button");
 btnReservar.addEventListener('click', validar);
 function validar(){
   cuerpo.style.backgroundColor = 'red';
