09a2ef9d91beaad095534d5ceb806dce52bf6627
R4f4S4lc3d0 <rafa22524@gmail.com>
2021-10-26 00:41:59 -0500

    Cambios perrones

--- ControlAdmin/CarpetaJS/FetchUsuario.js
+++ ControlAdmin/CarpetaJS/FetchUsuario.js
@@ -1,18 +1,18 @@
 const tabla = document.querySelector('#Tabla-Usuarios tbody');
 
 function cargarUsuarios(){
-        fetch('/CarpetaJson/usuarios.json')
+        fetch('http://localhost:8080/ApiRest/User')
         .then(respuesta => respuesta.json()) 
         .then(usuarios => {
             usuarios.forEach(usuario => {
                 const row = document.createElement('tr');
                 row.innerHTML += `
-                    <td>${usuario.id}</td>
+                    <td>${usuario.cliente_id}</td>
                     <td>${usuario.nombre}</td>
                     <td>${usuario.codigo}</td>
                     <td>${usuario.codigoC}</td>
-                    <td>${usuario.tel}</td>
-                    <td>${usuario.correo}</td>
+                    <td>${usuario.cel}</td>
+                    <td>${usuario.email}</td>
                     <td>${usuario.cumple}</td>
                     <td>${usuario.servicio}</td>
                     <td>${usuario.hora}</td>
