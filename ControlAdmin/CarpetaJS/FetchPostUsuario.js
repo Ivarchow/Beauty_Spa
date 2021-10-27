0c0aef6834abb934fa3daf485dc9ba6290018eda
CandyPacheco <89792773+CandyPacheco@users.noreply.github.com>
2021-10-26 21:45:23 -0500

    intento de fetch
    #1

---
+++ ControlAdmin/CarpetaJS/FetchPostUsuario.js
@@ -0,0 +1,9 @@
+const addPostUser = document.querySelector('.addPostUser');
+
+// Create - Insert new user
+//Method: POST
+
+addPostUser.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch('http://localhost:8080/ApiRest/User')
+})
