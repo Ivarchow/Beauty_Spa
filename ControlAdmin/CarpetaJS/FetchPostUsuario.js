107176164a429e5a58c87b668d252a2764049de1
CandyPacheco <89792773+CandyPacheco@users.noreply.github.com>
2021-10-27 13:18:45 -0500

    lloranding

--- ControlAdmin/CarpetaJS/FetchPostUsuario.js
+++ ControlAdmin/CarpetaJS/FetchPostUsuario.js
@@ -47,7 +47,31 @@ addPostUser.addEventListener('submit', (e) => {
     },
     body: JSON.stringify({
 
-
+"orden_id": 1,
+        "orden_date": "2021-10-27 00:00:00",
+        "fecha_reserva": "11/11/2021",
+        "hora_reserva": "3:00",
+        "cliente_id": {
+            "cliente_id": 1,
+            "cel": "3222638841",
+            "nombre": "Candy Pacheco",
+            "email": "Candy4@gmail.com",
+            "contraseña": "sdverwetgerfq",
+            "cumple": "15/08/1998",
+            "genero": "M",
+            "foto_perfil": " ",
+            "fecha_registro": "2021-10-27 00:00:00"
+        },
+        "id": {
+            "id": 2,
+            "titulo": "GELISH FRANCÉS",
+            "duracion": "60",
+            "precio": "280",
+            "img": " ",
+            "descripcion": "APLICACIÓN DE ESMALTE SEMIPERMANENTE  EN BASE TRANSPARENTE DE UÑA NATURAL,  CON LA PUNTA EN COLOR BLANCO O  CUALQUIER OTRO COLOR DE LA GAMA",
+            "categoria": "nails",
+            "carrito": "0"
+        
     })
   })  
   .then(respuesta => respuesta.json()) 
